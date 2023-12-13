3b33f19087a18c3898acfbc2cec04615352eb101
Shay Palachy <shaypal5@users.noreply.github.com>
2023-12-13 21:57:19 +0200

    Test on python 3.12 + versioneer fix

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
