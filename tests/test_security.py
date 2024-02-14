7a2a94d42c997f71fe787da48bff0c6f82c888b7
Jirka <jirka.borovec@seznam.cz>
2024-02-14 11:27:04 +0100

    ci: replace dead test with GH actions

--- tests/test_security.py
+++ tests/test_security.py
@@ -10,11 +10,3 @@ def test_bandit():
     subprocess.check_call(command + parameters)
     parameters = ["-s", "B101,B311,B404,B603", "-r", "tests"]
     subprocess.check_call(command + parameters)
-
-
-@pytest.mark.skip  # todo: dead check, enable it in separate PR w/ pre-commit
-def test_safety():
-    """Safety security scan passes with no warnings or errors."""
-    command = ["safety"]
-    parameters = ["check", "--full-report"]
-    subprocess.check_call(command + parameters)
