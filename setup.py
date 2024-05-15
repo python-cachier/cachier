044a6aace7f391891cef498bc7e7f18222b2e607
Jirka <jirka.borovec@seznam.cz>
2024-05-15 09:57:02 +0200

    pull back setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+"""Setup file for the Cachier package."""
+
+# This file is part of Cachier.
+# https://github.com/python-cachier/cachier
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2016, Shay Palachy <shaypal5@gmail.com>
+# Copyright (c) 2024, Jirka Borovec <***@gmail.com>
+
+from setuptools import setup
+
+setup()
