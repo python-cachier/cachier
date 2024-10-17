9dd89b00c57a88854acc6c4703767e48b9212b0a
Jirka Borovec <6035284+Borda@users.noreply.github.com>
2024-10-17 19:38:40 +0200

    convert `_default_params` to dataclass (#237)
    * convert `_default_params` to dataclass
    
    * lint
    
    * copy
    
    * set
    
    * keys
    
    * assert
    
    * global
    
    * global func
    
    * warning
    
    * fix import
    
    * lint
    
    ---------
    
    Co-authored-by: Shay Palachy-Affek <shaypal5@users.noreply.github.com>

--- src/cachier/__init__.py
+++ src/cachier/__init__.py
@@ -2,15 +2,15 @@ from ._version import *  # noqa: F403
 from .config import (
     disable_caching,
     enable_caching,
-    get_default_params,
-    set_default_params,
+    get_global_params,
+    set_global_params,
 )
 from .core import cachier
 
 __all__ = [
     "cachier",
-    "set_default_params",
-    "get_default_params",
+    "set_global_params",
+    "get_global_params",
     "enable_caching",
     "disable_caching",
 ]
